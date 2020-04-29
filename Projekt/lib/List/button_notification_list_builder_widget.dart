a774572b863c03c5759a1fe3146bb31386c40196
Darexad <291940@stud.umk.pl>
2020-04-29 13:50:45 +0200

    listbuilder z przyciskiem

---
+++ Projekt/lib/List/button_notification_list_builder_widget.dart
@@ -0,0 +1,69 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class BuildList extends StatefulWidget {
+
+  List<dynamic> lista;
+
+  @override
+  _BuildListState createState() => _BuildListState();
+
+}
+
+class _BuildListState extends State<BuildList> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+                shrinkWrap: true,
+                itemCount: widget.lista.length,
+                itemBuilder: (context, index) {
+                  return Row(
+                    children: <Widget>[
+                    RaisedButton(
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 50.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Container(
+                                child: Row(
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.account_circle,
+                                      size: 18.0,
+                                    ),
+                                    Text(widget.lista[index].nazwa),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    color: widget.lista[index].isSelected ? Colors.amber[400] : Colors.grey,
+                    onPressed: () => setState(() { 
+                      if(widget.lista[index].isSelected == false)
+                        widget.lista[index].isSelected = true;
+                      else
+                      widget.lista[index].isSelected = false;
+                      })
+              ),
+              new IconButton(
+                icon: Icon(Icons.block),
+                onPressed: () {widget.lista.removeAt(index);},
+                ),
+                    ],);
+                  
+                    });
+}
+}
