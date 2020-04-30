2e656b4b1f6779163d2d42f3a748931478cb0bcf
Darexad <291940@stud.umk.pl>
2020-04-30 20:19:27 +0200

    Poprawa listy notifi

--- Projekt/lib/List/button_notification_list_builder_widget.dart
+++ Projekt/lib/List/button_notification_list_builder_widget.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:pageview/Baza_danych/notification_helper.dart';
 import 'package:pageview/Classes/Notifi.dart';
+import 'package:duration/duration.dart';
 
 class ListNotifi extends StatefulWidget {
   List<Notifi> lista;
@@ -23,45 +24,61 @@ class _ListNotifiState extends State<ListNotifi> {
         shrinkWrap: true,
         itemCount: widget.lista.length,
         itemBuilder: (context, index) {
-          return Row(
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              RaisedButton(
-                  child: Container(
-                    alignment: Alignment.center,
-                    height: 50.0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            Container(
-                              child: Row(
-                                children: <Widget>[
-                                  Icon(
+              new RaisedButton(
+                child: Container(
+                  height: 50.0,
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Container(
+                                  width: 20,
+                                  child: Icon(
                                     Icons.account_circle,
                                     size: 18.0,
                                   ),
-                                  Text(widget.lista[index].duration.toString()),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
+                                ),
+                                SizedBox(
+                                  width: 80,
+                                ),
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width - 300,
+                                  child: Text(printDuration(widget.lista[index].duration, abbreviated: true) + " przed"),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                                  width: 30,
+                                ),
+                          SizedBox(
+                            child: IconButton(
+                              icon: Icon(Icons.clear),
+                              onPressed: () {
+                                if (widget.lista[index].id != null) {
+                                  NotifiHelper.delete(widget.lista[index].id);
+                                }
+                                widget.lista.removeAt(index);
+                                setState(() {});
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
+                ),
                 color: Colors.amber[400],
-
-              ),
-              new IconButton(
-                icon: Icon(Icons.clear),
-                onPressed: () {
-                  if(widget.lista[index].id != null) {
-                    NotifiHelper.delete(widget.lista[index].id);
-                  }
-                  widget.lista.removeAt(index);
-                  setState(() {});
-                },
               ),
             ],
           );
