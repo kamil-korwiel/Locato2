a05374c88f5311cf3e334d01e56c30db84d36319
Filip <fkowalski@hotmail.com>
2020-05-08 09:40:19 +0200

    lokalizacja notyfikacja

--- Projekt/lib/main.dart
+++ Projekt/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:pageview/Baza_danych/task_helper.dart';
 import 'package:pageview/pages/Add/add_event.dart';
 import 'package:pageview/pages/Add/add_task.dart';
 import 'package:pageview/pages/GroupPage/GroupPage.dart';
@@ -9,6 +11,8 @@ import 'package:pageview/pages/grouptaskpage.dart';
 
 import 'Background/notification_helper_background.dart';
 import 'Baza_danych/database_helper.dart';
+import 'Baza_danych/localization_helper.dart';
+import 'Classes/Localization.dart';
 import 'pages/calendar.dart';
 
 void main() async {
@@ -16,6 +20,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Notifications_helper_background.initialize();
   await AndroidAlarmManager.initialize();
+
+  await AndroidAlarmManager.periodic(
+      Duration(minutes: 2), 0, checkLocationRadius);
   runApp(MyApp());
 }
 
@@ -43,7 +50,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           primarySwatch: Colors.grey,
           appBarTheme: AppBarTheme(color: Color(0xFF332F53)),
-          accentColor:  Colors.amberAccent,
+          accentColor: Colors.amberAccent,
           canvasColor: Color.fromRGBO(51, 47, 83, 1),
           bottomAppBarColor: Color(0xFF333366),
           brightness: Brightness.dark,
@@ -94,118 +101,117 @@ class _HomePageState extends State<HomePage>
     var screenWidthTabBar = MediaQuery.of(context).size.width * 0.8;
 
     return Scaffold(
-        appBar: AppBar(
-            //title: Text("Locato"),
-            leading: GestureDetector(
-              onTap: () {},
-              //child: Icon(Icons.menu),
-            ),
-            elevation: 0.0,
-            flexibleSpace: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [const Color(0xFF534B83), const Color(0xFF332F53)],
-                  begin: const FractionalOffset(0.0, 0.0),
-                  end: const FractionalOffset(0.0, 1.0),
-                  stops: [0.0, 0.7],
-                  tileMode: TileMode.clamp,
-                ),
+      appBar: AppBar(
+          //title: Text("Locato"),
+          leading: GestureDetector(
+            onTap: () {},
+            //child: Icon(Icons.menu),
+          ),
+          elevation: 0.0,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [const Color(0xFF534B83), const Color(0xFF332F53)],
+                begin: const FractionalOffset(0.0, 0.0),
+                end: const FractionalOffset(0.0, 1.0),
+                stops: [0.0, 0.7],
+                tileMode: TileMode.clamp,
               ),
             ),
-            bottom: PreferredSize(
-              preferredSize: Size(screenWidthTabBar, 5.0),
-              child: new Container(
-                width: screenWidthTabBar,
-                child: new TabBar(
-                  indicatorSize: TabBarIndicatorSize.label,
-                  indicatorColor: Color(0xff00c6ff),
-                  controller: _tabController,
-                  tabs: <Widget>[
-                    new Container(
-                      height: 40.0,
-                      child: new Tab(
-                        text: "Kalendarz".toUpperCase(),
-                      ),
+          ),
+          bottom: PreferredSize(
+            preferredSize: Size(screenWidthTabBar, 5.0),
+            child: new Container(
+              width: screenWidthTabBar,
+              child: new TabBar(
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorColor: Color(0xff00c6ff),
+                controller: _tabController,
+                tabs: <Widget>[
+                  new Container(
+                    height: 40.0,
+                    child: new Tab(
+                      text: "Kalendarz".toUpperCase(),
                     ),
-                    new Container(
-                      height: 40.0,
-                      child: new Tab(text: "Tydzień".toUpperCase()),
-                    ),
-                    new Container(
-                      height: 40.0,
-                      child: new Tab(text: "Grupy".toUpperCase()),
-                    ),
-                  ],
-                ),
+                  ),
+                  new Container(
+                    height: 40.0,
+                    child: new Tab(text: "Tydzień".toUpperCase()),
+                  ),
+                  new Container(
+                    height: 40.0,
+                    child: new Tab(text: "Grupy".toUpperCase()),
+                  ),
+                ],
               ),
-            )),
-        floatingActionButton: SpeedDial(
-          elevation: 10.0,
-          animatedIcon: AnimatedIcons.add_event,
-          animatedIconTheme: IconThemeData(size: 22.0),
-          closeManually: false,
-          overlayColor: Colors.black,
-          backgroundColor: Color(0xFF444477),
-          overlayOpacity: 0.5,
-          onOpen: () => print('Otwieram Dial na Tasks'),
-          onClose: () => print('Zamykam Dial na Tasks'),
-          heroTag: 'speed-dial-hero-tag',
-          children: [
-            SpeedDialChild(
-                child: Icon(Icons.event_note),
-                backgroundColor: Color(0xFF444477),
-                label: 'Wydarzenie',
-                labelStyle: TextStyle(color: Colors.grey[900], fontSize: 18.0),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AddEvent(),
-                    ),
-                  );
-                }),
-            SpeedDialChild(
-                child: Icon(Icons.check_box),
-                backgroundColor: Color(0xFF444477),
-                label: 'Zadanie',
-                labelStyle: TextStyle(color: Colors.grey[900], fontSize: 18.0),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AddTask(),
-                    ),
-                  );
-                  setState(() {});
-                }),
-            SpeedDialChild(
-              child: Icon(Icons.data_usage),
-              label: 'DB',
+            ),
+          )),
+      floatingActionButton: SpeedDial(
+        elevation: 10.0,
+        animatedIcon: AnimatedIcons.add_event,
+        animatedIconTheme: IconThemeData(size: 22.0),
+        closeManually: false,
+        overlayColor: Colors.black,
+        backgroundColor: Color(0xFF444477),
+        overlayOpacity: 0.5,
+        onOpen: () => print('Otwieram Dial na Tasks'),
+        onClose: () => print('Zamykam Dial na Tasks'),
+        heroTag: 'speed-dial-hero-tag',
+        children: [
+          SpeedDialChild(
+              child: Icon(Icons.event_note),
+              backgroundColor: Color(0xFF444477),
+              label: 'Wydarzenie',
               labelStyle: TextStyle(color: Colors.grey[900], fontSize: 18.0),
               onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddEvent(),
+                  ),
+                );
+              }),
+          SpeedDialChild(
+              child: Icon(Icons.check_box),
+              backgroundColor: Color(0xFF444477),
+              label: 'Zadanie',
+              labelStyle: TextStyle(color: Colors.grey[900], fontSize: 18.0),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddTask(),
+                  ),
+                );
+                setState(() {});
+              }),
+          SpeedDialChild(
+            child: Icon(Icons.data_usage),
+            label: 'DB',
+            labelStyle: TextStyle(color: Colors.grey[900], fontSize: 18.0),
+            onTap: () {
+              // Notifications_helper_background.now("NOW", "FUCK");
 
-               // Notifications_helper_background.now("NOW", "FUCK");
-
-                DatabaseHelper.instance.showalltables();
-              },
-            ),
-          ],
-        ),
-        body: PageView(
-          controller: _pageController,
-          onPageChanged: (page) {
-            if (pageCanChange) {
-              onPageChange(page);
-            }
-            //_tabController.animateTo(page);
-          },
-          children: <Widget>[
-            Calendar(),
-            HomePageEvents(),
-            GroupTaskPage(),
-          ],
-        ),
-      );
+              DatabaseHelper.instance.showalltables();
+            },
+          ),
+        ],
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (page) {
+          if (pageCanChange) {
+            onPageChange(page);
+          }
+          //_tabController.animateTo(page);
+        },
+        children: <Widget>[
+          Calendar(),
+          HomePageEvents(),
+          GroupTaskPage(),
+        ],
+      ),
+    );
   }
 
   onPageChange(int index, {PageController p, TabController t}) async {
@@ -220,4 +226,43 @@ class _HomePageState extends State<HomePage>
       _tabController.animateTo(index);
     }
   }
+}
+
+void checkLocationRadius() async {
+  List<Localization> _list;
+  double dist;
+  Position pos;
+  int id = -1;
+  int distance = 100;
+
+  pos = await Geolocator().getCurrentPosition();
+  print("Obecna lokalizacja: " + pos.toString());
+
+  // DownloadData
+  LocalizationHelper.lists().then((onList) {
+    if (onList != null) {
+      _list.addAll(onList);
+    }
+  });
+
+  if (_list != null) {
+    for (var loc in _list) {
+      // Dystans w metrach pomiedzy dwoma punktami
+      dist = await Geolocator().distanceBetween(
+          loc.latitude, loc.longitude, pos.latitude, pos.longitude);
+      print("Dystans pomiedzy punktami" + dist.toString());
+      // Sprawdz czy znajduje sie w poblizu lokalizacja
+      if (dist < distance) {
+        // Jestes w okregu o promieniu 'distance' metrow
+        id = loc.id;
+        print("Id zadania w poblizu: " + id.toString());
+      }
+    }
+  }
+
+  _list.clear();
+
+  /*if (id >= 0) {
+    TaskHelper.listsIDLocal(id).th
+  }*/
 }
